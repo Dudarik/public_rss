77b3339821110b00f405b75becb5b480e4d6c78f
Dudarik <Dudarik@yandex.ru>
2022-10-04 20:02:42 +0500

    feat: add burger menu to donate page

--- online-zoo/assets/js/main.js
+++ online-zoo/assets/js/main.js
@@ -1,9 +1,6 @@
 const burger_toggle_button = document.querySelector(".burger__toggle-button");
 const overlay = document.querySelector(".burger__menu_overlay");
 
-// console.log(burger_toggle_button);
-// if (!burger_toggle_button) return;
-
 burger_toggle_button.addEventListener("click", () => {
   burger_toggle_button.classList.toggle("burger__menu_active");
 });
