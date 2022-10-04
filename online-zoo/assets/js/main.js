1f9e5c1902b70d38984ebe577e767b48477842af
Dudarik <Dudarik@yandex.ru>
2022-10-04 19:57:35 +0500

    feat: burger menu ready to work

--- online-zoo/assets/js/main.js
+++ online-zoo/assets/js/main.js
@@ -1,13 +1,13 @@
-const burger_menu = document.querySelector(".menu__burger");
+const burger_toggle_button = document.querySelector(".burger__toggle-button");
 const overlay = document.querySelector(".burger__menu_overlay");
 
-console.log(burger_menu);
-// if (!burger_menu) return;
+// console.log(burger_toggle_button);
+// if (!burger_toggle_button) return;
 
-burger_menu.addEventListener("click", () => {
-  burger_menu.classList.toggle("burger__menu_active");
+burger_toggle_button.addEventListener("click", () => {
+  burger_toggle_button.classList.toggle("burger__menu_active");
 });
 
 overlay.addEventListener("click", () => {
-  burger_menu.classList.remove("burger__menu_active");
+  burger_toggle_button.classList.remove("burger__menu_active");
 });
