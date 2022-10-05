5ddfc68f4139caa34161a21b1a5844d040c3d4c3
Dudarik <Dudarik@yandex.ru>
2022-10-05 20:58:26 +0500

    feat: add stop scroll

--- online-zoo/assets/js/main.js
+++ online-zoo/assets/js/main.js
@@ -1,10 +1,13 @@
 const burger_toggle_button = document.querySelector(".burger__toggle-button");
 const overlay = document.querySelector(".burger__menu_overlay");
+const body = document.querySelector("body");
 
 burger_toggle_button.addEventListener("click", () => {
   burger_toggle_button.classList.toggle("burger__menu_active");
+  body.classList.toggle("burger__stop-scroll");
 });
 
 overlay.addEventListener("click", () => {
   burger_toggle_button.classList.remove("burger__menu_active");
+  body.classList.remove("burger__stop-scroll");
 });
