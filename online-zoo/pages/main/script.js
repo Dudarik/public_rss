9d61ec210fc25f3af1996fea59064e90a2ba5ad9
Dudarik <Dudarik@yandex.ru>
2022-10-06 13:17:13 +0500

    feat: add popup overlay, card

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,4 +1,5 @@
 "use strict";
+const store = { renderCards: {} };
 
 /**
  * @params
@@ -16,45 +17,51 @@ const testimonialsSliderCards = document.querySelector(
   "#testimonials_slider_cards"
 );
 
+const popupOverlay = document.querySelector("#popup_overlay");
+
+const popupCard = document.querySelector(".popup__card");
+
 async function getTestimonialsData() {
   const result = await fetch("db_testimonials.json")
     .then((response) => response.json())
     .then((data) => data);
   return result;
 }
+
 async function generateRndTestimonialsCards(countCards) {
   const allCards = await getTestimonialsData();
 
-  let renderCards = document.querySelector("#testimonials_slider_cards");
+  let $renderCards = document.querySelector("#testimonials_slider_cards");
 
-  renderCards.innerHTML = "";
+  $renderCards.innerHTML = "";
 
   const idsArray = new Array(allCards.length).fill(0).map((_, id) => id);
 
   for (let i = 0; i < countCards; i++) {
-    console.log(idsArray);
+    // console.log(idsArray);
     const cardId = idsArray[getRandomCardId(idsArray.length - 1)];
-    console.log(cardId);
+    // console.log(cardId);
 
     idsArray.splice(
       idsArray.findIndex((val) => val === cardId),
       1
     );
-    // console.log(idsArray.slice());
+
+    store.renderCards[cardId] = { ...allCards[cardId] };
 
     const card_tpl = document.querySelector("#testimonialsSliderCard_tpl");
 
     const currentCard = card_tpl.content.cloneNode(true);
+
+    currentCard
+      .querySelector(".slider__card")
+      .setAttribute("data-cardid", cardId);
 
     const avatar = currentCard.querySelector(".user-avatar > img");
     const userName = currentCard.querySelector(".user-name");
     const userLoacation = currentCard.querySelector(".user__location");
     const dateTimePost = currentCard.querySelector(".date-time__post");
     const cardText = currentCard.querySelector(".card__text");
-    // console.log(userLoacation);
-
-    console.log(allCards[cardId]);
-    // console.log(allCards[idsArray[cardId]].avatar);
 
     avatar.src = allCards[cardId].avatar;
     userName.innerText = allCards[cardId].name;
@@ -62,10 +69,8 @@ async function generateRndTestimonialsCards(countCards) {
     dateTimePost.innerText = allCards[cardId].dateTime;
     cardText.innerText = allCards[cardId].body;
 
-    renderCards.appendChild(currentCard);
+    $renderCards.appendChild(currentCard);
   }
-
-  // console.log(idsArray);
 }
 generateRndTestimonialsCards(11);
 
@@ -82,11 +87,29 @@ testimonialsProgress.addEventListener("input", () => {
   testimonialsSliderCards.style.left = `${
     -1 * oneOffset * testimonialsProgress.value
   }px`;
-  // console.log(testimonialsProgress.value);
 });
 
+popupOverlay.addEventListener("click", (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  console.log(event.target);
+
+  popupOverlay.classList.remove("popup__overlay_active");
+});
+
+popupCard.addEventListener("click", (event) => {
+  event.stopPropagation();
+  if (event.target.id === "close_popup") {
+    popupOverlay.classList.remove("popup__overlay_active");
+  }
+});
+
+testimonialsSliderCards.addEventListener("click", (event) => {
+  popupOverlay.classList.toggle("popup__overlay_active");
+});
 // const testimonialCards = [
 //   {
+//     id: 0,
 //     avatar: "../../assets/images/testimonials/avatars/avatar1.png",
 //     name: "Vasya Pupkin",
 //     localiton: "Austria",
@@ -94,6 +117,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate.",
 //   },
 //   {
+//     id: 1,
 //     avatar: "../../assets/images/testimonials/avatars/avatar2.png",
 //     name: "Robert De Niro",
 //     localiton: "Germany",
@@ -101,6 +125,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Take a walk on the wild side! Become a docent volunteer at the L.A. Zoo. Our docent class offers in-depth zoology, ecology, and conservation training, focusing on the animals and plants at the Zoo.",
 //   },
 //   {
+//     id: 2,
 //     avatar: "../../assets/images/testimonials/avatars/avatar3.png",
 //     name: "Jack Nicholson",
 //     localiton: "Austria",
@@ -108,20 +133,23 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Let’s Take Flight! Come soar through the Zoo with our avian touring docents on another egg-cellent adventure. Which group of birds REALLY has green and red feathers and why? Which species of bird has the largest eye of any living land animal? Which eagle has the biggest beak (hint: the L.A. Zoo has a mated pair)? Why is Cyrano the scarlet ibis a different color from the others? Which birds seal their mates in trees? Come find out on October 22, and enjoy an autumn morning with the Zoo’s birds.",
 //   },
 //   {
-//     avatar: "../../assets/images/testimonials/avatars/avatar_1.jpg",
+//     id: 3,
+//     avatar: "../../assets/images/testimonials/avatars/avatar_1.png",
 //     name: "Meryl Streep",
 //     localiton: "Germany",
 //     dateTime: "Today",
 //     body: "Come hoof it at the Zoo with us! Stride through the Zoo with our hoofstock, hyrax, and elephant touring docents on a fabulous, four-legged, walking safari. Which antelope don’t begin to pant until the temperature exceeds 112 degrees? Which hoofed animal can run 55 mph for up to two hours because it evolved to outrun North American cheetahs? What’s the difference between horns and antlers? What physical movement is impossible for elephants, but fairly easy for many humans? Why do some animals chew their cud? What is a “dassie?” Come find out on November 12, and enjoy an autumn morning with the Zoo’s hoofstock, hyraxes, and elephants.",
 //   },
 //   {
-//     avatar: "../../assets/images/testimonials/avatars/avatar2.jpg",
+//     id: 4,
+//     avatar: "../../assets/images/testimonials/avatars/avatar_2.jpg",
 //     name: "Vasya Pupkin",
 //     localiton: "Austria",
 //     dateTime: "Yesterday",
 //     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ut ad labore dolore odit voluptate.",
 //   },
 //   {
+//     id: 5,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_3.jpg",
 //     name: "Tom Hanks",
 //     localiton: "Austria",
@@ -129,6 +157,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Come join us for the Los Angeles Zoo’s autumn quarterly docent-led bird walk. In addition to our year-round residents, this is the time to spot birds that have come south from the Pacific Northwest and western Canada, such as white-crowned sparrows, American goldfinches and yellow-rumped warblers. The walk starts at 8 a.m. before the Zoo opens! We have a limited supply of mini binoculars to borrow, but if you have your own, please bring them, and be ready to spot birds as we “flock” together at the Zoo.",
 //   },
 //   {
+//     id: 6,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_4.png",
 //     name: "Elizabeth Taylor",
 //     localiton: "Germany",
@@ -136,6 +165,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate.",
 //   },
 //   {
+//     id: 7,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_5.jpg",
 //     name: "Vasya Pupkin",
 //     localiton: "Austria",
@@ -143,6 +173,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate.",
 //   },
 //   {
+//     id: 8,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_6.jpg",
 //     name: "Leonardo DiCaprio",
 //     localiton: "Austria",
@@ -150,6 +181,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Come prowl through the Zoo with our touring docents as we enjoy some of nature’s greatest hunters and the well adapted prey who evade them. Which predators lie in wait for an ambush, and which ones chase down each meal? How many ways are there to avoid getting eaten? More than you can imagine! What can an animal’s eyes tell you about its location in the food chain? Come find out on December 10, and enjoy a winter morning with the Zoo’s predators and prey",
 //   },
 //   {
+//     id: 9,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_7.png",
 //     name: "Vasya Pupkin",
 //     localiton: "Austria",
@@ -157,6 +189,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "sunt aut quae laboriosam sit ut impedit\nadipisci harum laborum totam deleniti voluptas odit rem ea\nnon iure distinctio ut velit doloribus\net non ex",
 //   },
 //   {
+//     id: 10,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_8.jpg",
 //     name: "Cate Blanchett",
 //     localiton: "France",
@@ -164,6 +197,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "consequuntur quia voluptate assumenda et\nautem voluptatem reiciendis ipsum animi est provident\nearum aperiam sapiente ad vitae iste\naccusantium aperiam eius qui dolore voluptatem et",
 //   },
 //   {
+//     id: 11,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_9.jpg",
 //     name: "Vasya Pupkin",
 //     localiton: "France",
@@ -171,6 +205,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "quia incidunt ut\naliquid est ut rerum deleniti iure est\nipsum quia ea sint et\nvoluptatem quaerat eaque repudiandae eveniet aut",
 //   },
 //   {
+//     id: 12,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_10.jpg",
 //     name: "Kate Winslet",
 //     localiton: "Austria",
@@ -178,6 +213,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "iste ut laborum aliquid velit facere itaque\nquo ut soluta dicta voluptate\nerror tempore aut et\nsequi reiciendis dignissimos expedita consequuntur libero sed fugiat facilis",
 //   },
 //   {
+//     id: 13,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_11.jpg",
 //     name: "Sophia Loren",
 //     localiton: "France",
@@ -185,6 +221,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "repudiandae repellat quia\nsequi est dolore explicabo nihil et\net sit et\net praesentium iste atque asperiores tenetur",
 //   },
 //   {
+//     id: 14,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_12.jpg",
 //     name: "Steve McQueen",
 //     localiton: "Austria",
@@ -192,6 +229,7 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "The best online zoo I’ve met. My son delighted very mucljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for",
 //   },
 //   {
+//     id: 15,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_13.jpg",
 //     name: "Julia Roberts",
 //     localiton: "France",
@@ -199,13 +237,15 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate.",
 //   },
 //   {
-//     avatar: "../../assets/images/testimonials/avatars/avatar_14.png",
+//     id: 16,
+//     avatar: "../../assets/images/testimonials/avatars/avatar_14.jpg",
 //     name: "Morgan Freeman",
 //     localiton: "Austria",
 //     dateTime: "Yesterday",
 //     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate.",
 //   },
 //   {
+//     id: 17,
 //     avatar: "../../assets/images/testimonials/avatars/avatar_15.jpg",
 //     name: "Helen Mirren",
 //     localiton: "France",
@@ -213,3 +253,5 @@ testimonialsProgress.addEventListener("input", () => {
 //     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ea laudantium facere debitis consequuntur ut ad labore dolore odit voluptate.",
 //   },
 // ];
+
+// console.log(JSON.stringify(testimonialCards));
