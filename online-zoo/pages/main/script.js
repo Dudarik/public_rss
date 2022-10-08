ce74265bf86f6100aa1eeae81eef9202920eb578
Dudarik <Dudarik@yandex.ru>
2022-10-08 14:10:02 +0500

    feat: add functions generate card and slide for pets carousel

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -43,6 +43,75 @@ window.addEventListener("scroll", () => {
   );
 });
 
+const generateNewPetsCard = (cardId) => {
+  const allCards = store.petsCards;
+
+  const $card_tpl = document.querySelector("#petsSliderCard_tpl");
+
+  const $currentCard = $card_tpl.content.cloneNode(true);
+
+  const pathToImages = {
+    desktop: "../../assets/images/desktop/pets/slider/",
+    small_desktop: "../../assets/images/small_desktop/pets/slider/",
+    tablet: "../../assets/images/tablet/pets/slider/",
+    mobile: "../../assets/images/mobile/pets/slider/",
+  };
+
+  const BASE_ICON_PATH = "../../assets/icons/cards_icons/";
+  const pathToIcons = {
+    desktop: BASE_ICON_PATH,
+    mobile: BASE_ICON_PATH + "mobile/",
+  };
+
+  const $desktop_img = $currentCard.querySelector("#slider_card_desktop_image");
+  const $small_desktop_img = $currentCard.querySelector(
+    "#slider_card_small_desktop_image"
+  );
+  const $tablet_img = $currentCard.querySelector("#slider_card_tablet_image");
+  const $mobile_img = $currentCard.querySelector("#slider_card_mobile_image");
+
+  const $cardTitle = $currentCard.querySelector(".card__title");
+  const $cardSubtitle = $currentCard.querySelector(".card__sub-title");
+
+  const $iconCardBig = $currentCard.querySelector("#icon_card_big");
+  const $iconCardSmall = $currentCard.querySelector("#icon_card_small");
+
+  $desktop_img.srcset = pathToImages.desktop + allCards[cardId].image;
+  $small_desktop_img.srcset =
+    pathToImages.small_desktop + allCards[cardId].image;
+  $tablet_img.srcset = pathToImages.tablet + allCards[cardId].image;
+  $mobile_img.srcset = pathToImages.mobile + allCards[cardId].image;
+
+  $cardTitle.innerText = allCards[cardId].name;
+  $cardSubtitle.innerText = allCards[cardId].location;
+
+  $iconCardBig.srcset = pathToIcons.desktop + allCards[cardId].icon;
+  $iconCardSmall.srcset = pathToIcons.mobile + allCards[cardId].icon;
+  return $currentCard;
+};
+
+const generateNewPetsSlide = (countCards, $slide) => {
+  const idsArray = new Array(store.petsCards.length).fill(0).map((_, id) => id);
+
+  for (let i = 0; i < countCards; i++) {
+    const cardId = idsArray[getRandomCardId(idsArray.length - 1)];
+    // console.log(idsArray, cardId);
+
+    idsArray.splice(
+      idsArray.findIndex((id) => id === cardId),
+      1
+    );
+
+    $slide.appendChild(generateNewPetsCard(cardId));
+  }
+};
+
+let center = document.querySelector(".slider__cards.center__slide");
+center.innerHTML = "";
+console.log(center);
+
+generateNewPetsSlide(6, center);
+
 /**
  *
  * @param {number} countCards
@@ -66,8 +135,6 @@ function generateRndTestimonialsCards(countCards) {
       1
     );
 
-    // store.testimonialCards[cardId] = { ...allCards[cardId] };
-
     const $card_tpl = document.querySelector("#testimonialsSliderCard_tpl");
 
     const $currentCard = $card_tpl.content.cloneNode(true);
@@ -81,7 +148,6 @@ function generateRndTestimonialsCards(countCards) {
     const $userLoacation = $currentCard.querySelector(".user__location");
     const $dateTimePost = $currentCard.querySelector(".date-time__post");
     const $cardText = $currentCard.querySelector(".card__text");
-    // console.log(allCards);
 
     $avatar.src = allCards[cardId].avatar;
     $userName.innerText = allCards[cardId].name;
@@ -163,9 +229,6 @@ const handleClosePopup = (event) => {
 
   popupOverlay.classList.remove("popup__overlay_active");
 };
-
-const generateNewCard = () => {};
-const generateNewSlide = (countCards) => {};
 
 popupOverlay.addEventListener("click", handleClosePopup);
 
