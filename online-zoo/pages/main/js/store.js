59f18472f34a1bed8736d4f69f3a0338cc4f0171
Dudarik <Dudarik@yandex.ru>
2022-10-09 15:15:54 +0500

    fix: for safari and firefox

--- online-zoo/pages/main/js/store.js
+++ online-zoo/pages/main/js/store.js
@@ -1,6 +1,8 @@
 import { petsSliderCardsCount } from "./helpers.js";
-import petsCards from "../db_animals.json" assert { type: "json" };
-import testimonialCards from "../db_testimonials.json" assert { type: "json" };
+import { petsCards } from "../db_animals.js";
+import { testimonialCards } from "../db_testimonials.js";
+// import petsCards from "../db_animals.json" assert { type: "json" };
+// import testimonialCards from "../db_testimonials.json" assert { type: "json" };
 
 export const store = {
   testimonialCards,
