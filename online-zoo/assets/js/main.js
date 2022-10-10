716b3750b76041bdce2a9c8be8b53348a02d569d
Dudarik <Dudarik@yandex.ru>
2022-10-10 19:53:05 +0500

    fix: overlay in menu and popup

--- online-zoo/assets/js/main.js
+++ online-zoo/assets/js/main.js
@@ -1,6 +1,8 @@
 const burger_toggle_button = document.querySelector(".burger__toggle-button");
 const overlay = document.querySelector(".burger__menu_overlay");
 
+let scrollY = "0px";
+
 burger_toggle_button.addEventListener("click", () => {
   burger_toggle_button.classList.toggle("burger__menu_active");
   burger_toggle_button.classList.contains("burger__menu_active")
@@ -18,28 +20,34 @@ window.addEventListener("resize", () => {
 
   if (width > 998) {
     const popupOverlay = document.querySelector("#popup_overlay");
-
     burger_toggle_button.classList.remove("burger__menu_active");
     popupOverlay.classList.remove("popup__overlay_active");
     startScroll();
+    const body = document.body;
+    body.style.position = "";
   }
 });
 
 export const stopScroll = () => {
-  const scrollY = document.documentElement.style.getPropertyValue("--scroll-y");
+  const currentOffsetY =
+    document.documentElement.style.getPropertyValue("--scroll-y");
+  if (
+    parseInt(currentOffsetY) &&
+    parseInt(currentOffsetY) !== parseInt(scrollY)
+  )
+    scrollY = currentOffsetY;
+
   const body = document.body;
-  const header = document.querySelector("header");
-  body.style.position = "fixed";
+  body.classList.add("body_fixed");
+
   body.style.top = `-${scrollY}`;
-  header.style.top = `${scrollY - window.innerHeight}`;
 };
 
 export const startScroll = () => {
   const body = document.body;
-  const scrollY = body.style.top;
-  const header = document.querySelector("header");
-  body.style.position = "static";
+  const currY = parseInt(scrollY) * -1 + "px"; //body.style.top;
+  body.classList.remove("body_fixed");
+  body.style.position = "";
   body.style.top = "";
-  header.style.top = 0;
-  window.scrollTo(0, parseInt(scrollY || "0") * -1);
+  window.scrollTo(0, parseInt(currY || "0") * -1);
 };
