88a24445b020fb761e8edba50c90e3b88183276b
Dudarik <Dudarik@yandex.ru>
2022-10-10 14:36:05 +0500

    fix: bugs in burger menu

--- online-zoo/assets/js/main.js
+++ online-zoo/assets/js/main.js
@@ -1,13 +1,33 @@
 const burger_toggle_button = document.querySelector(".burger__toggle-button");
 const overlay = document.querySelector(".burger__menu_overlay");
-const body = document.querySelector("body");
 
 burger_toggle_button.addEventListener("click", () => {
   burger_toggle_button.classList.toggle("burger__menu_active");
-  body.classList.toggle("burger__stop-scroll");
+  burger_toggle_button.classList.contains("burger__menu_active")
+    ? stopScroll()
+    : startScroll();
 });
 
 overlay.addEventListener("click", () => {
   burger_toggle_button.classList.remove("burger__menu_active");
-  body.classList.remove("burger__stop-scroll");
+  startScroll();
 });
+
+export const stopScroll = () => {
+  const scrollY = document.documentElement.style.getPropertyValue("--scroll-y");
+  const body = document.body;
+  const header = document.querySelector("header");
+  body.style.position = "fixed";
+  body.style.top = `-${scrollY}`;
+  header.style.top = `${scrollY - window.innerHeight}`;
+};
+
+export const startScroll = () => {
+  const body = document.body;
+  const scrollY = body.style.top;
+  const header = document.querySelector("header");
+  body.style.position = "static";
+  body.style.top = "";
+  header.style.top = 0;
+  window.scrollTo(0, parseInt(scrollY || "0") * -1);
+};
