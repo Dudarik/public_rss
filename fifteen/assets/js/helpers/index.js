0e843e1456ca2a2e09984fc9ee61b3005a8b7c60
Dudarik <Dudarik@yandex.ru>
2022-10-18 21:58:33 +0500

    feat: add some helpers

---
+++ fifteen/assets/js/helpers/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} min
+ * @param {number} max
+ *
+ * @return {number}
+ */
+
+export const getRandomNum = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+/**
+ * @param {void}
+ * @return {number} from 0 to 3
+ */
+
+export const getRandomShift = () => getRandomNum(0, 3);
