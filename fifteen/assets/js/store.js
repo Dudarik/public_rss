306e4f7cf223662d4feaab085759cd64952f5dba
Dudarik <Dudarik@yandex.ru>
2022-10-18 21:48:19 +0500

    feat: add more settings in store

--- fifteen/assets/js/store.js
+++ fifteen/assets/js/store.js
@@ -1,13 +1,22 @@
 export const store = {
+  gameArray: [],
+  gameSettings: {
+    sound: false,
+    numbers: true,
+    timerEnable: false,
+  },
+
   records: [],
 
   playTime: "",
-  stepCount: 0,
+  movesCount: 0,
 
   dragableStartPosX: 0,
   dragableStartPosY: 0,
   dragableEndPosX: 0,
   dragableEndPosY: 0,
+
+  animationDropableTime: 500,
 
   dropablePosX: 0,
   dropablePosY: 0,
