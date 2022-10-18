90588348ce260d2e8342edc58b5e9fc035359bd1
Dudarik <Dudarik@yandex.ru>
2022-10-18 21:37:56 +0500

    feat: add store to project

---
+++ fifteen/assets/js/store.js
@@ -0,0 +1,14 @@
+export const store = {
+  records: [],
+
+  playTime: "",
+  stepCount: 0,
+
+  dragableStartPosX: 0,
+  dragableStartPosY: 0,
+  dragableEndPosX: 0,
+  dragableEndPosY: 0,
+
+  dropablePosX: 0,
+  dropablePosY: 0,
+};
