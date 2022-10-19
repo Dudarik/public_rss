787e52ddbeae1043ae6816d84285994cbd44c8a6
Dudarik <dudarik@yandex.ru>
2022-10-19 09:19:26 +0500

    feat: add board settings

--- fifteen/assets/js/store.js
+++ fifteen/assets/js/store.js
@@ -1,9 +1,25 @@
+const BOARD_SIZE_3 = 3;
+const BOARD_SIZE_4 = 4;
+const BOARD_SIZE_5 = 5;
+const BOARD_SIZE_6 = 6;
+const BOARD_SIZE_7 = 7;
+const BOARD_SIZE_8 = 8;
+
 export const store = {
   gameArray: [],
   gameSettings: {
     sound: false,
     numbers: true,
     timerEnable: false,
+    boardSizes: [
+      BOARD_SIZE_3,
+      BOARD_SIZE_4,
+      BOARD_SIZE_5,
+      BOARD_SIZE_6,
+      BOARD_SIZE_7,
+      BOARD_SIZE_8,
+    ],
+    currentBoardSize: BOARD_SIZE_4,
   },
 
   ls_available: false,
