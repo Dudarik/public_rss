0bb52137c05260759b823a9f74f28b4949a11217
Dudarik <dudarik@yandex.ru>
2022-10-23 08:41:22 +0500

    refactor: remove hours from time

--- fifteen/assets/js/helpers/index.js
+++ fifteen/assets/js/helpers/index.js
@@ -16,15 +16,15 @@ export const getRandomNum = (min, max) =>
 export const getRandomShift = () => getRandomNum(0, 3);
 
 export const formatTime = (time = 0) => {
-  let hours = parseInt(time / 3600, 10);
+  // let hours = parseInt(time / 3600, 10);
   let minutes = parseInt((time % 3600) / 60, 10);
   let seconds = parseInt((time % 3600) % 60, 10);
 
-  hours = hours < 10 ? "0" + hours : String(hours);
+  // hours = hours < 10 ? "0" + hours : String(hours);
   minutes = minutes < 10 ? "0" + minutes : String(minutes);
   seconds = seconds < 10 ? "0" + seconds : String(seconds);
 
-  return `${hours}:${minutes}:${seconds}`;
+  return `${minutes}:${seconds}`; //${hours}:
 };
 
 export const incrementTimer = (time) => time + 1;
