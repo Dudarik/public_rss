d7d5ae574c5343904dbe1aa0bcd89d5fd0fb4067
Dudarik <Dudarik@yandex.ru>
2022-11-12 14:30:17 +0500

    init: start songbird project! Good luck and have fun!

---
+++ songbird/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.[name].[chunkhash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|jpg|jpeg|png|webp|svg|mp3)$/i,
+        type: 'asset/resource', //asset/resource
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: 'css/bundle.[name].[chunkhash].css' }),
+  ],
+};
