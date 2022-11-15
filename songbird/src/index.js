41270d69daa2d86fd2033b5093097683ca998aef
Dudarik <Dudarik@yandex.ru>
2022-11-15 10:41:41 +0500

    feat: test referrer

--- songbird/src/index.js
+++ songbird/src/index.js
@@ -1,11 +1,21 @@
 // import './assets/js/helpers/location.js';
 import './assets/sass/styles.scss';
-const BASE_NAME =
-  'https://rolling-scopes-school.github.io/dudarik-JSFE2022Q3/songbird/';
+// const BASE_NAME = '/dudarik-JSFE2022Q3/songbird/';
+// https://rolling-scopes-school.github.io/
 console.log('working');
-
+console.log(document.referrer);
 window.addEventListener('popstate', () => {
   changePage(location.pathname);
+});
+window.addEventListener('load', () => {
+  if (!document.referrer) {
+    changePage('main.html');
+    history.pushState(null, null, 'main.html');
+  }
+  if (document.referrer) {
+    changePage(document.referrer);
+    history.pushState(null, null, document.referrer);
+  }
 });
 
 const getMenuItems = () => {
@@ -29,7 +39,7 @@ const changePage = async (href) => {
 
 const addHandlersToMenu = (links) => {
   links.forEach((link) => {
-    console.log(link);
+    // console.log(link);
     // if (link.id === 'menu_main') return;
     link.addEventListener('click', async (event) => {
       event.preventDefault();
