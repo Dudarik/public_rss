527a53e521b325414895566153fca665361ae6d6
Dudarik <Dudarik@yandex.ru>
2022-12-06 13:14:54 +0500

    feat: add some default settings

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '1d57e164c9ec47d7bd0c099d627af853', // получите свой ключ https://newsapi.org/
         });
     }
 }
