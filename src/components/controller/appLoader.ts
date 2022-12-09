83ca19ae95ed80b505e2322cf3d9d806e177d2ee
Dudarik <Dudarik@yandex.ru>
2022-12-09 15:34:45 +0500

    fix: change path to proxy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            // super('https://nodenews.up.railway.app', {
+        // super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '1d57e164c9ec47d7bd0c099d627af853', // получите свой ключ https://newsapi.org/
         });
     }
