39695e053ac0e946afb44854f079869e25cf5e71
Dudarik <Dudarik@yandex.ru>
2023-01-19 11:24:59 +0500

    feat: add store

---
+++ src/store.ts
@@ -0,0 +1,8 @@
+export const store = {
+  cars: [],
+  inGame: false,
+  totalCarsCount: 0,
+  currentGaragePage: 0,
+  currentWinnersPage: 0,
+  winnersTable: [],
+};
---
+++ src/ts/interfaces/store.ts
@@ -0,0 +1,10 @@
+import { Car } from './cars';
+
+export interface Store {
+  cars: Car[];
+  inGame: boolean;
+  totalCarsCount: number;
+  currentGaragePage: number;
+  currentWinnersPage: number;
+  winnersTable: Car[];
+}
