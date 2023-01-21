43080255e845489bf435e87d5f1e96273235d7da
Dudarik <dudarik@yandex.ru>
2023-01-21 09:57:17 +0500

    refactor: gen100 cars functions

--- src/ts/lib/generateCars.ts
+++ src/ts/lib/generateCars.ts
@@ -1,15 +1,21 @@
 import { store } from '../../store';
 import { createCar } from '../api/apiCars';
+import { Car } from '../interfaces/cars';
 import { getRandomColor, getRandomFromZero } from './getRandom';
 
-export const generateCars = (count: number) => {
+export const generateCars = async (count: number) => {
   if (count < 1) throw new Error(`Can't create Zero car or low`);
+  const newCarsArr: Omit<Car, 'id'>[] = [];
 
   for (let i = 0; i < count; i += 1) {
     const brand = store.carsBrands[getRandomFromZero(store.carsBrands.length - 1)];
     const model = store.carsModels[getRandomFromZero(store.carsModels.length - 1)];
     const color = getRandomColor();
 
-    createCar({ name: `${brand} ${model}`, color });
+    newCarsArr.push({ name: `${brand} ${model}`, color });
   }
+
+  const request = newCarsArr.map((car) => createCar(car));
+
+  await Promise.allSettled(request);
 };
--- src/ts/lib/handlers/handlerGen100CarsClick.ts
+++ src/ts/lib/handlers/handlerGen100CarsClick.ts
@@ -1,6 +1,19 @@
+import { store } from '../../../store';
+import { garagePage } from '../../pages/garage/garagePage';
 import { generateCars } from '../generateCars';
+import { initStore } from '../initStore';
 
 export const handlerGen100CarsClick = async (event: Event) => {
   event.preventDefault();
-  generateCars(100);
+  await generateCars(100);
+
+  await initStore();
+  const garagePageHTML = garagePage();
+  const main = document.querySelector('#app');
+
+  if (!(main instanceof HTMLElement)) throw new Error(`Can't find main section`);
+
+  main.innerHTML = '';
+  main.append(garagePageHTML);
+  console.log('log', store.countCars);
 };
